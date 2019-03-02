9b43261c0fc1a87baf937af7d14e6033f24fa040
David Kitchen <david@buro9.com>
2019-03-02 17:25:23 +0000

    go mod

--- vendor/github.com/yosssi/ace-proxy/doc.go
+++
@@ -1,5 +0,0 @@
-// Package proxy provides a proxy for the Ace template engine.
-// The proxy caches the options for the Ace template engine
-// so that you don't have to specify them every time calling
-// the Ace APIs.
-package proxy
