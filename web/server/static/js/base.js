dcd0ce0736bf4899e695d919ecbe22e45fa5c0d7
Pascal Opitz <contact@pascalopitz.com>
2022-09-09 13:27:22 +0930

    Moved legacy JS code into base_legacy.js file for clarity

--- web/server/static/js/base.js
+++ web/server/static/js/base.js
@@ -172,37 +172,3 @@
 
 	document.addEventListener('DOMContentLoaded', onDomReadyHandler, false);
 })();
-
-////////////////////
-//	   tooltip    //
-////////////////////
-(function () {
-	$('[data-toggle=tooltip]').tooltip({
-		container: 'body'
-	});
-})();
-
-////////////////////
-//   btn-groups   //
-////////////////////
-(function () {
-
-	var btn_groups = '.btn-group';
-
-	var toggleButtonParent = function (e) {
-		var self = $(e.currentTarget),
-			siblings = $('input[name="' + self.attr('name') + '"]'),
-			activeClass = 'active';
-
-		if (self.is(':checked')) {
-			siblings.parent().removeClass(activeClass);
-			self.parent().addClass(activeClass);
-		} else {
-			self.parent().removeClass(activeClass);
-		}
-
-	}
-
-	$(btn_groups).on('change', 'input[type=radio]', toggleButtonParent);
-
-})();
